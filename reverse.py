b4dae3648eac3456221b2d998261de1487905249
Vixaary <vixaary@gmail.com>
2024-10-20 00:45:31 +0600

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,14 @@
+def func(r):
+    return r[::-1]
+r = "Python"
+print(func(r))
+
+def substring(my_string):
+    return my_string
+my_string = "hello world"
+print(my_string[0:5])
+
+def spaces(t):
+    return spaces
+t = "hello world"
+print(t[0:11])
